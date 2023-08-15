3d8ca0609da0288780a645b7e2de6d7bf5ba454d
Mohamed Ahmed <mohammedhassan9748.com>
2023-08-15 16:58:41 +0300

    Updating unit 2

---
+++ Unit_2/L3_C_Basics/Home_Work_1/EX2.c
@@ -0,0 +1,16 @@
+/*
+ * EX.c
+ *
+ *  Created on: Aug 14, 2023
+ *      Author: Mohamed Ahmed
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("%s","##########Console-output###\n");
+	int x=0;
+	scanf("%d",&x);
+	printf("%d",x);
+	return 0;
+}
