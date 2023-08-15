2d3366a925045cca45dddadec25ee6b6e10dc711
Mohamed Ahmed <mohammedhassan9748.com>
2023-08-15 04:02:23 +0300

    Adiing Unit 2 Folder

---
+++ Unit_2/L3_C_Basics/Home_Work_1/EX1.c
@@ -0,0 +1,17 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Aug 14, 2023
+ *      Author: Mohamed Ahmed
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("%s","##########Console-output###\n");
+	printf("%s","C Programming\n");
+	printf("%s","###########################");
+	return 0;
+}
+
+
